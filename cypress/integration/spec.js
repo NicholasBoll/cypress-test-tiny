036240a9be2c5a5f530e481fd57a6b8214ec27ec
Nicholas Boll <nicholas.boll@gmail.com>
2020-08-12 16:51:52 -0600

    Update spec.js

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,10 @@
-/// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  beforeEach(() => {
+    cy.visit("cypress/fixtures/index.html");
+  });
+
+  it("should trigger the click action when typing {enter}", () => {
+    cy.get("button").type("{enter}");
+    cy.get("output").should("contain", "Clicked!");
+  });
+});
