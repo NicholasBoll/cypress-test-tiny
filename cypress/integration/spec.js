0ba4a15197d6bad9af2914f94958f6ec852b4525
Nicholas Boll <nicholas.boll@gmail.com>
2020-12-13 17:42:22 -0700

    Updated spec to show for typing random characters
    A new spec now shows typing anything in an unfocused element will trigger a click

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,8 +3,13 @@ describe("page", () => {
     cy.visit("cypress/fixtures/index.html");
   });
 
-  it("should trigger the click action when typing {enter}", () => {
+  it("should trigger a click action when typing {enter}", () => {
     cy.get("button").focus().type("{enter}");
     cy.get("output").should("contain", "Clicked!");
+  });
+
+  it("should not trigger a click action when typing random characters", () => {
+    cy.get("button").type("foobar");
+    cy.get("output").should("not.contain", "Clicked!");
   });
 });
